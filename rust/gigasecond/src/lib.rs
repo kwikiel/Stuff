d10f22d2584c4f967ffed1fbeffd668243b90b9c
Kacper Wikieł <k.wikiel@gmail.com>
2021-07-19 10:41:35 +0200

    refactor: return statement not required ( like in Ruby )

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -3,5 +3,5 @@ use chrono::{DateTime, Utc, Duration};
 const GIGASECOND: i64 = 1_000_000_000; 
 
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    return start + Duration::seconds(GIGASECOND);
+    start + Duration::seconds(GIGASECOND);
 }
